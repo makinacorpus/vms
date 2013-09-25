ae3481176e71457fda1f2f50976d5cdad77e2f85
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-09-25 19:50:35 +0200

    salt vms

---
+++ ubuntu/mastersalt/postinst.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export SALT_BOOT="mastersalt"
+cd /tmp
+wget http://raw.github.com/makinacorpus/makina-states/master/_scripts/boot-salt.sh -O - | bash
+ps aux|grep salt-master|awk '{print $2}'|xargs kill -9
+ps aux|grep salt-minion|awk '{print $2}'|xargs kill -9
+find /etc/*salt*/pki -type f -delete
+rm -rf /var/cache/apt/archives/*deb
+# vim:set et sts=4 ts=4 tw=80:
