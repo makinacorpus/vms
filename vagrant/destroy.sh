9c1e8cd98c7ede91d4082347851535b036f998f4
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-10-16 18:55:59 +0200

    fix: desotry

--- vagrant/destroy.sh
+++ vagrant/destroy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Are you really sure ? (controlc to abort)"
 read
-rm -rf .vb_name .vagrant salt
 vagrant halt -f
 vagrant destroy
+rm -rf .vb_name .vagrant salt
 vagrant up
 # vim:set et sts=4 ts=4 tw=80:
