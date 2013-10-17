64caa0fd1aa7283424c4baab90b2c11c04c31b5a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-10-17 12:39:15 +0200

    better full docker reset

--- docker/rmall.sh
+++ docker/rmall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-for j in $(docker ps -a |awk '{print $1}');do 
+for j in $(docker ps -a |awk '{print $1}'|grep -v ID);do 
     echo $j
     docker stop -t=1  $j
     docker rm  $j
 done
+docker images -a|egrep  '<none>.*<none>'|awk '{print $3}'|xargs docker rmi
 service docker restart
 # vim:set et sts=4 ts=4 tw=80:
