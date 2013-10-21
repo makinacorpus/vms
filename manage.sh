b7f5647c8644f03685698b7002ec2217ac171282
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-10-21 16:44:59 +0200

    export/import working

--- manage.sh
+++ manage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-actions="up reload destroy down export import suspend"
+actions="up reload destroy down export import import_nude suspend"
 RED="\\033[31m"
 CYAN="\\033[36m"
 NORMAL="\\033[0m"
@@ -39,7 +39,11 @@ down() {
 }
 up() {
     cd $c
-    if [[ "$(not_created)" == "1" ]] || [[ "$1" != "noreload" ]];then
+    local noreload=""
+    if [[ "$1" == "noreload" ]];then
+        noreload=y
+    fi
+    if [[ "$(not_created)" == "1" ]] || [[ -z $noreload ]];then
         log "First run, we issue an up & reload first"
         vagrant up
         vagrant reload && no_up=1
@@ -59,63 +63,165 @@ reload() {
 }
 export() {
     cd $c
-    log "Exporting to $c/package.tar.bz2"
-    up &&\
-        vagrant ssh &&\
-        sed -ire "s/^SUBSYSTEM/#SUBSYSTEM/g" \
-            /etc/udev/rules.d/70-persistent-net.rules &&\
-        down &&\
-        sed -ire 's/config\.vm\.box\s*=.*/config.vm.box = "devhost"/g' Vagrantfile &&\
-        vagrant package --include .vb_* pillar projects salt --vagrantfile Vagrantfile
-        #log "package produced in $c/package.box"
-        #log "Compressing box and marker files in $c/package.tar.bz2" &&\
-        #tar cjvf package.tar.bz2 package.box  &&\
-        #rm -vf package.box &&\
-        #log "package produced in $c/package.tar.bz2"
+    local nincludes=""
+    local includes=""
+    local gtouched=""
+    for i in $@;do
+        if [[ $i == "nosed" ]];then
+            nosed=y
+        fi
+    done
+    nincludes=""
+    for i in .vb_* vagrant_config.rb;do
+        if [[ -e "$i" ]];then
+            nincludes="$i $nincludes"
+        fi
+    done
+    includes="$nincludes"
+    for i in pillar projects salt;do
+        if [[ -e $i ]];then
+            includes="$i $includes"
+        fi
+    done
+    log "Exporting to $c/package.box"
+    tar_preopts="cjvf"
+    tar_postopts="--numeric-owner"
+    if [[ $(uname) == "Darwin" ]];then
+        tar_postopts=""
+    fi
+    if [[ $(uname) != "Darwin" ]];then
+        tar_preopts="${tar_preopts}p"
+    fi
+    if [[ ! -f package-full.box ]];then
+        if [[ -z $nosed ]];then \
+            up noreload &&\
+            vagrant ssh \
+               -c 'sudo sed -ire "s/^SUBSYSTEM/#SUBSYSTEM/g" /etc/udev/rules.d/70-persistent-net.rules';\
+        fi &&\
+        down;\
+        sed -ie 's/config\.vm\.box\s*=.*/config.vm.box = "devhost"/g' Vagrantfile &&\
+        log "Be patient, exporting now the full box" &&\
+        vagrant package --vagrantfile Vagrantfile --output package-full.box
+    else
+        log "$c/package-full.box exists, delete it to redo"
+    fi &&\
+    gtouched="1" &&\
+    if [[ ! -f package-full.tar.bz2 ]];then
+        log "Be patient, archiving now the whole full box package" &&\
+        tar $tar_preopts package-full.tar.bz2 package-full.box $includes $tar_postopts &&\
+        log "Export done of full box: $c/package-full.tar.bz2"
+    else
+        log "$c/package-full.tar.bz2 exists, delete it to redo"
+    fi &&\
+    if [[ ! -f package-nude.box ]];then
+        log "Be patient, exporting now the nude box" &&\
+        vagran package --vagrantfile Vagrantfile --output package-nude.box
+    else
+        log "$c/package-nude.box exists, delete it to redo"
+    fi &&\
+    if [[ ! -f package-nude.tar.bz2 ]];then
+        log "Be patient, archiving now the whole nude box package" &&\
+        tar $tar_preopts package-nude.tar.bz2 package-nude.box $nincludes $tar_postopts  &&\
+        log "Export done of nude box: $c/package-nude.tar.bz2"
+    else
+        log "$c/package-nude.tar.bz2 exists, delete it to redo"
+    fi
     ret=$?
     # reseting Vagrantfile in any case
-    git checkout Vagrantfile 2>/dev/null
+    if [[ -n $gtouched ]];then
+        git checkout Vagrantfile 2>/dev/null
+    fi
     if [[ $ret != 0 ]];then
         log "Error while exporting"
         exit $ret
+    else
+        log "package produced in $c/package.box"
     fi
+}
+import_nude() {
+    import nude
 }
 import() {
     cd $c
-    log "Importing $c/package.tar.bz2"
-    #if [[ ! -e package.tar.bz2 ]];then
-    #    log "Missing $c/package.tar.bz2"
-    #    exit -1
+    local gtouched=""
+    tar="sudo tar xjvpf --numeric-owner"
+    tar="sudo tar xjvpf"
+    mode=""
+    args=${@}
+    amodes="full nude"
+    for amode in $amodes;do
+        for arg in $@;do
+            if [[ $mode == $args ]] && [[ -z $mode ]];then
+                mode=$arg
+            fi
+        done
+    done
+    if [[ -z $mode ]];then
+        for i in $amodes;do
+            if [[ -e package-$i.tar.bz2 ]];then
+                mode=$i
+                log "No specified mode, defaulting to $mode"
+                break
+            fi
+        done
+    fi
+    if [[ -z $mode ]];then
+        log "No import archive found"
+        exit -1
+    fi
+    box="$c/package-$mode.box"
+    arc="$c/package-$mode.tar.bz2"
+    tar_preopts="-xjvpf"
+    tar_postopts="--numeric-owner"
+    #if [[ $(uname) == "Darwin" ]];then
+    #    tar_postopts=""
     #fi
-    #if [[ ! -e package.box ]];then
-    #    log "Unarchiving package.tar.bz2"
-    #    tar xjvf "$c/package.tar.bz2"
-    #    if [[ $? != 0 ]];then
-    #        log "Error unarchiving package.tar.bz2"
-    #    fi
-    #    if [[ ! -e package.box ]];then
-    #        log "Missing $c/package.box"
-    #        exit -1
-    #    fi
-    #else
-    #    log "Existing $c/package.box, if you want to re dearchive, delete it"
-    #fi
-
-    log "Importing box" && vagrant box add -f devhost package.box &&\
-    log "Initialiasing host from package.box" &&\
-    sed -ire 's/config\.vm\.box\s*=.*/config.vm.box = "devhost"/g' \
-    Vagrantfile && up noreload && down && git checkout Vagrantfile &&\
-    log "Box imported !"
+    if [[ ! -e "$arc" ]];then
+        log "Missing $arc"
+        exit -1
+    fi
+    if [[ ! -e "$box" ]];then
+        log "Unarchiving $arc"
+        # need to sudo to restore sticky GID
+        sudo tar $tar_preopts "$arc" $tar_postopts
+        if [[ $? != 0 ]];then
+            log "Error unarchiving $arc"
+        fi
+        if [[ ! -e "$box" ]];then
+            log "Missing $box"
+            exit -1
+        fi
+    else
+        log "Existing $box, if you want to unarchive again, delete it"
+    fi
+    if [[ -f "./vagrant_config.rb" ]];then
+        EXPORTED_VM="$(ruby  -e 'require File.expand_path("./vagrant_config.rb");include MyConfig;printf("%s", VIRTUALBOX_VM_NAME)' 2>/dev/null)"
+    fi
+    if [[ $(VBoxManage list vms|grep "\"$EXPORTED_VM\""|wc -l) != "0" ]];then
+        log "This virtualbox already exists, please rename or delete it"
+        VBoxManage list vms|grep "\"$EXPORTED_VM\""
+        uid=$(VBoxManage list vms|grep "\"$EXPORTED_VM\""|awk -F'{' '{print $2}'|sed -e 's/}//g')
+        log "You can try VBoxManage modifyvm $uid --name \"sav_$EXPORTED_VM\""
+    fi
+    log "Importing $box (mode: $mode) into vagrant bases boxes as 'devhost' box" &&\
+    vagrant box add -f devhost "$box" &&\
+    log "Initialiasing host from $box" &&\
+    sed -ie 's/config\.vm\.box\s*=.*/config.vm.box = "devhost"/g' \
+    Vagrantfile;gtouched="1" && up noreload && down
     ret=$?
     # reseting Vagrantfile in any case
-    git checkout Vagrantfile 2>/dev/null
+    if [[ -n $gtouched ]];then
+        git checkout Vagrantfile 2>/dev/null
+    fi
     if [[ $ret != 0 ]];then
-        log "Error while importing"
+        log "Error while importing $box"
         exit $ret
+    else
+        log "Box $box imported !"
     fi
 }
 action=$1
-test="$(echo "$actions" | sed -re "s/.* $action .*/match/g")"
+test="$(echo "$actions" | sed -e "s/.* $action .*/match/g")"
 if [[ "$test" == "match" ]];then
     shift
     $action $@
