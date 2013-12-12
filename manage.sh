31105c2a2eda6ef305a49c750c5aa39c0f785d4c
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-12-12 01:16:54 +0100

    test mode

--- manage.sh
+++ manage.sh
@@ -29,7 +29,7 @@ test() {
     name=$(grep ' UBUNTU_RELEASE="' Vagrantfile|sed -e 's/.*="//' -e 's/"//g')
     echo $name
     d="$c-test"
-    sudo rsync -azv $c/ $d/ --exclude=salt/ --exclude=pillar --exclude=projects --exclude=.vagrant --exclude=vagrant_config.rb --exclude=docker/
+    sudo rsync -azv $c/ $d/ --exclude=salt/ --exclude=pillar --exclude=projects --exclude=.vagrant --exclude=packer --exclude=vagrant_config.rb --exclude=docker/
     cd ${d} || exit -1
     ./manage.sh destroy
     rm -rf salt projects pillar .vagrant
